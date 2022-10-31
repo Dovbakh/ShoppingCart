b82b3d82c295b4345e80d93ddf6f337e59deb20c
Svyatoslav <dovbakhs@yandex.ru>
2022-10-31 15:18:21 +0300

    Added Dto Class

---
+++ src/ShoppingCart/Contracts/ShoppingCart.Contracts/ShoppingCartDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Contracts
+{
+    public class ShoppingCartDto
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProductName { get; set; }
+
+
+
+    }
+}
